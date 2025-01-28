8c489e7e9625e5943b55810e9d6fc89deedad442
ウェスリー <wesley@evokateur.net>
2025-01-28 13:21:42 -0800

    use one to many paragraphs for cover letter instead of specific sections

--- fill-cover-letter-template.py
+++ fill-cover-letter-template.py
@@ -8,11 +8,12 @@ def json_to_latex_template(json_data, template_file="cover-letter-template.tex",
     template = template.replace("xXemail", json_data["contact"]["email"])
     template = template.replace("xXphone", json_data["contact"]["phone"])
     template = template.replace("xXphone", json_data["contact"]["phone"])
-    template = template.replace("xXintroduction", json_data["introduction"])
-    template = template.replace("xXeducation", json_data["education"])
-    template = template.replace("xXqualities", json_data["qualities"])
-    template = template.replace("xXexperience", json_data["experience"])
-    template = template.replace("xXclosing", json_data["closing"])
+
+    paragraphs = ""
+    for paragraph in json_data["paragraphs"]:
+        paragraphs += paragraph + "\n\n"
+        
+    template = template.replace("xXparagraphs", paragraphs)
     
     with open(output_file, "w") as file:
         file.write(template)
