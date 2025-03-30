946c00c617321807fb38f06c2ee8d1491309ae71
ウエスリー <wesley@evokateur.net>
2025-03-29 18:53:57 -0700

    add script to clean up generated and ignored files

---
+++ clean.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd `dirname "$0"`
+
+rm -r *.aux
+rm -r *.fdb_latexmk
+rm -r *.fls
+rm -r *.log
+rm -r *.out
+rm -r *.pdf
+rm -r *.swp
+rm -r *.synctex.gz
+rm -r cv.tex
+rm -r cover-letter.tex
