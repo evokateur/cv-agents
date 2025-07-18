04c65625b4f104d53f3af9f8b84c01723b00f02b
ウエスリー <wesley@sfizio.evokateur.net>
2025-07-18 13:59:51 -0700

    add func for tex jinja env

--- fill-cv-template.py
+++ fill-cv-template.py
@@ -1,6 +1,19 @@
 import json
 from jinja2 import Environment, FileSystemLoader
 
+
+def get_tex_env():
+    return Environment(
+        loader=FileSystemLoader("."),
+        block_start_string="(%",
+        block_end_string="%)",
+        variable_start_string="((",
+        variable_end_string="))",
+        comment_start_string="(#",
+        comment_end_string="#)",
+    )
+
+
 with open("cv.json") as f:
     data = json.load(f)
 
