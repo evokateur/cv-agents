b60503a1d02e195f472fd065e787465f8c21cae3
ウエスリー <wesley@turnip.evokateur.net>
2025-08-01 15:02:26 -0700

    update setup.sh, put jupyter kernel in venv

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@
 python -m venv .venv --clear
 source .venv/bin/activate
 pip install -r requirements.txt
-python -m ipykernel install --user --name=cv-agent --display-name "CV Agent"
+python -m ipykernel install --user --env VIRTUAL_ENV $(pwd)/.venv --name=cv-agent --display-name "CV Agent"
