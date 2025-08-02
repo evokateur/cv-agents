9993b8c9a83b32e5b599fb616327fc3ef836a381
ウエスリー <wesley@cipolla.evokateur.net>
2025-08-02 01:03:07 -0700

    verbosity in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+echo "[Re]creating virtual environment and installing dependencies.."
 python -m venv .venv --clear
 source .venv/bin/activate
 pip install -r requirements.txt
-python -m ipykernel install --user --env VIRTUAL_ENV $(pwd)/.venv --name=cv-agent --display-name "CV Agent"
+
+echo "Installing Jupyter kernel spec for virtual environment.."
+python -m ipykernel install --user --env VIRTUAL_ENV "$(pwd)/.venv" --name=cv-agent --display-name "CV Agent"
