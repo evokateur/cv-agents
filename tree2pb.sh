25542e38c1387820b41f14be66c5da0be0e17072
ウエスリー <wesley@sfizio.evokateur.net>
2025-08-15 11:21:34 -0700

    fix up tree2pb.sh

--- tree2pb.sh
+++ tree2pb.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-tree -I 'resources|vector_db|__pycache__|knowledge-base|__init__*|examples|*.aux|*.log|*.md|Makefile|data|output|*.sh|*.out|*.ipynb'|pbcopy
+tree -I '_docs|vector_db|__pycache__|knowledge-base|__init__*|examples|*.aux|*.log|*.md|Makefile|data|output|*.sh|*.out|*.ipynb' | sed '$d; $d' | tee >(pbcopy)
+
+echo "(copied to paste buffer)"
