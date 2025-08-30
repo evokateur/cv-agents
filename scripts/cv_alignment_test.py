09f8c6f92ed0658dabbae50dfcb922970b9a47f0
ウエスリー <wesley@cipolla.evokateur.net>
2025-08-29 18:14:01 -0700

    update output dir in test script to match branch

--- scripts/cv_alignment_test.py
+++ scripts/cv_alignment_test.py
@@ -2,7 +2,7 @@ import json
 import optimizer.kickoff as kickoff
 
 candidate_cv_path = "data/cv.yaml"
-output_directory = "job_postings/tests/new-job-analysis"
+output_directory = "job_postings/tests/cv-transformer"
 
 
 def test_cv_alignment_with_config():
